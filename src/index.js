176fb60b42c4f2f97c71ae8528e8796adaa52ac7
Mayur Mendhe <mayurmendhe28@gmail.com>
2022-04-03 17:20:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const connect = require("./config/db");
 
 const port = process.env.PORT || 5000;
@@ -10,12 +10,14 @@ app.use(express.json());
 
 const userController = require("./controllers/user.controller");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use("/user" , userController);
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
     email : {type : String , required : true},
-    password : {type : String , required : true },
-    username : {type : String, required : true},
-    gender : {type : String ,required : true},
+    password : {type : String },
+    username : {type : String},
+    gender : {type : String },
 
 })
 
